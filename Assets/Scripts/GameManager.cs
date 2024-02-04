0e0fd0afc7252749fa7c768d4c02099b1b7d0124
Weston Mcnamara <westonfmcnamara@gmail.com>
2024-02-03 23:13:53 -0500

    Changed gamemanager to be a singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,6 +28,22 @@ public class GameManager : NetworkBehaviour
         set { gameModeTypeToLoad = value; }
     }
 
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
